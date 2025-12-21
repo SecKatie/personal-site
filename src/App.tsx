74fc06edceecbc21348f24c4da5c2c0e47e04566
Katie Mulliken <katie@mulliken.net>
2025-12-21 12:24:45 -0500

    fix(routes): put layout around routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,12 @@ import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './content/Home.mdx'
 
-function HomePage() {
-  return (
-    <Layout>
-      <Home />
-    </Layout>
-  )
-}
-
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Layout>
   )
 }
